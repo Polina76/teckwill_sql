562926c2ab8a350dbe1ecbbaa37bff5b4b4bcf84
Polina Popovici <ppopovici@mail.ru>
2018-05-18 20:46:25 +0300

    comentariu

---
+++ Lesson5/Oracle_Cloud_DB.sql
@@ -0,0 +1,22 @@
+--select * from all_tables
+
+--Select distinct SALARY
+--DELETE FROM table WHERE ,DEPARTMENT_ID 
+--,COMMISSION_PCT
+--,EMPLOYEE_ID
+--,FIRST_NAME
+--from employees
+
+--select first_name
+--,last_name
+--,employee_ID
+--,salary*1.1
+--from employees
+
+/*comentariu pentru mai multe rinduri*/
+-- comentariu pentru un rind
+
+select * from employees
+
+DESCRIBE EMPLOYEES
+
